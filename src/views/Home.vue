8959bb660394e00468856e0654676c8bb3053dd1
kimutaiRop <kimrop19@gmail.com>
2021-06-13 12:02:32 +0300

    add refresh button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <HomeComponent/>
+  <div>
+    <button v-if="rem_seconds < 30000" @click="refreshToken">
+      refresh token
+    </button>
+    token expires in {{ parseInt(rem_seconds / 1000) }} sec
+    <HomeComponent/>
+  </div>
+
 </template>
 <script>
 // @ is an alias to /src
 import HomeComponent from '@/components/HomeComponent.vue'
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex";
 
 export default {
   name: "Home",
